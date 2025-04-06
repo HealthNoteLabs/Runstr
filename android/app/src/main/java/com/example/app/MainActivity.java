eceb0ad2977b70f870cf772b354a9e44a519ba13
thewildhustle <thewildhustle@proton.me>
2025-04-06 10:41:06 -0400

    Black Screen

--- android/app/src/main/java/com/example/app/MainActivity.java
+++ android/app/src/main/java/com/example/app/MainActivity.java
@@ -1,12 +1,25 @@
 package com.example.app;
 
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.util.Log;
+
 import com.getcapacitor.BridgeActivity;
+import com.getcapacitor.Plugin;
 import com.example.app.pedometer.StepCounterPlugin;
 
 public class MainActivity extends BridgeActivity {
+    private static final String TAG = "RunstrApp";
+
     @Override
-    public void onCreate(android.os.Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
+        // Enable WebView debugging
+        WebView.setWebContentsDebuggingEnabled(true);
+        
+        // Log app startup for debugging
+        Log.d(TAG, "Runstr application starting...");
         
         // Register our plugins
         registerPlugin(StepCounterPlugin.class);
